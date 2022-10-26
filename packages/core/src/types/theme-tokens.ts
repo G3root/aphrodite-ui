be4726fac3b1b9a4847702f17cfcaa494cf2b766
nafees nazik <nafeesnazik21@gmail.com>
2022-10-27 00:45:44 +0530

    feat: add types

--- packages/core/src/types/theme-tokens.ts
+++ packages/core/src/types/theme-tokens.ts
@@ -1,7 +1,7 @@
 export type Colors = 'pri' | 'dan' | 'suc' | 'war' | 'inf' | 'neu';
 export type Properties = 'bg' | 'hov-bg' | 'txt' | 'bor' | 'hov-bor';
 export type Components = 'btn';
-export type Variants = 'sld' | 'flt' | 'out' | 'gho' | 'lin';
+export type BtnVariants = 'sld' | 'flt' | 'out' | 'gho' | 'lin';
 
-export type TokenValue = `${Components}-${Variants}-${Colors}-${Properties}`;
-export type TokenKey = `--${Components}-${Variants}-${Colors}-${Properties}`;
+export type TokenValue = `${Components}-${BtnVariants}-${Colors}-${Properties}`;
+export type TokenKey = `--${Components}-${BtnVariants}-${Colors}-${Properties}`;
