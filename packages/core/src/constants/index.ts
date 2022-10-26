9fbe2971aa3804696d9d7c281c3f87619b6162b8
nafees nazik <nafeesnazik21@gmail.com>
2022-10-27 00:46:04 +0530

    feat: add constants

---
+++ packages/core/src/constants/index.ts
@@ -0,0 +1,13 @@
+import type {
+  Colors as ColorsType,
+  BtnVariants as BtnVariantsType
+} from '../types';
+
+export const Colors: ColorsType[] = ['dan', 'inf', 'neu', 'pri', 'suc', 'war'];
+export const BtnVariants: BtnVariantsType[] = [
+  'flt',
+  'gho',
+  'lin',
+  'out',
+  'sld'
+];
