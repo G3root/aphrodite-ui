4c744b6e28087860b5ed5e69cf082677b2fe5f80
nafees nazik <nafeesnazik21@gmail.com>
2022-10-27 00:47:43 +0530

    feat: add button styles to plugin

--- packages/core/src/plugin/index.ts
+++ packages/core/src/plugin/index.ts
@@ -1,9 +1,13 @@
 import plugin from 'tailwindcss/plugin';
 import { darkTheme, lightTheme } from '../theme';
+import { button } from '../components';
 
-export const aphroditePlugin = plugin(({ addBase }) => {
+export const aphroditePlugin = plugin(({ addBase, addComponents }) => {
   addBase({
     ':root': lightTheme,
     '[data-theme="dark"]': darkTheme
+  });
+  addComponents({
+    ...button
   });
 });
