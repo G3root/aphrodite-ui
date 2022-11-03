5fdd9c322113a08cc5da716341a6c9e8ccd82bfd
nafees nazik <nafeesnazik21@gmail.com>
2022-11-03 18:03:17 +0530

    feat: add content layer configs

---
+++ apps/docs/contentlayer.config.ts
@@ -0,0 +1,56 @@
+import {
+  ComputedFields,
+  defineDocumentType,
+  makeSource
+} from 'contentlayer/source-files';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
+
+import { siteConfig } from './src/configs';
+
+import { getTableOfContents } from './src/utils';
+
+const computedFields: ComputedFields = {
+  slug: {
+    type: 'string',
+    resolve: (doc) => `/${doc._raw.flattenedPath}`
+  },
+  editUrl: {
+    type: 'string',
+    resolve: (doc) => `${siteConfig.repo.editUrl}/${doc._id}`
+  }
+};
+
+const Doc = defineDocumentType(() => ({
+  name: 'Doc',
+  filePathPattern: 'docs/**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    description: { type: 'string', required: true }
+  },
+  computedFields: {
+    ...computedFields,
+    frontMatter: {
+      type: 'json',
+      resolve: (doc) => ({
+        title: doc.title,
+        description: doc.description,
+        category: doc._raw.sourceFileDir.replace('docs/components/', ''),
+        slug: `/${doc._raw.flattenedPath}`,
+        headings: getTableOfContents(doc.body.raw),
+        editUrl: `${siteConfig.repo.editUrl}/${doc._id}`
+      })
+    }
+  }
+}));
+
+const contentLayerConfig = makeSource({
+  contentDirPath: 'src/pages',
+  documentTypes: [Doc],
+  mdx: {
+    remarkPlugins: [rehypeSlug, remarkGfm]
+  }
+});
+
+export default contentLayerConfig;
