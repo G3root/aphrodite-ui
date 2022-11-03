0658ebba8f205e0cbe8745c8c319541ac7f9294e
nafees nazik <nafeesnazik21@gmail.com>
2022-11-03 18:02:36 +0530

    feat(docs): add site config

---
+++ apps/docs/src/configs/site-config.ts
@@ -0,0 +1,9 @@
+const baseUrl = 'https://github.com/G3root/aphrodite-ui';
+
+export const siteConfig = {
+  repo: {
+    url: baseUrl,
+    editUrl: `${baseUrl}/edit/main/apps/docs/src/pages`,
+    blobUrl: `${baseUrl}/edit/main/apps/docs/src/pages`
+  }
+};
