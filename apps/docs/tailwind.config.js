52d6fba334b9da6b198261bafe74ea1b617911f6
nafees nazik <nafeesnazik21@gmail.com>
2022-11-04 17:04:25 +0530

    chore: add aphrodite ui plugin

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,9 +1,14 @@
+const { aphroditePlugin } = require('@aphrodite-ui/core');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@aphrodite-ui/react/src/**/*.{js,ts,jsx,tsx}'
+  ],
   darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [aphroditePlugin()]
 };
