b8ad22ae3977bd1e2ed9ce48b12654764cf56677
nafees nazik <nafeesnazik21@gmail.com>
2022-11-09 00:04:54 +0530

    chore(docs): add mdx to tailwind content

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -3,7 +3,7 @@ const { aphroditePlugin } = require('@aphrodite-ui/core');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@aphrodite-ui/react/src/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: ['class', '[data-theme="dark"]'],
