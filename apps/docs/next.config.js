91090a29514eb8af5b664c19a02ca4cf073e38e6
nafees nazik <nafeesnazik21@gmail.com>
2022-11-08 20:27:25 +0530

    chore(docs): add redirection

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,7 +3,21 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/docs/getting-started',
+        permanent: false
+      },
+      {
+        source: '/docs',
+        destination: '/docs/getting-started',
+        permanent: false
+      }
+    ];
+  }
 };
 
 module.exports = withContentlayer(nextConfig);
