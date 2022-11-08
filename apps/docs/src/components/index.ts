c7c2527955ff8249b962943cd082f81498a61557
nafees nazik <nafeesnazik21@gmail.com>
2022-11-09 00:09:22 +0530

    refactor(docs): move mdxComponents into a seperate file

--- apps/docs/src/components/index.ts
+++ apps/docs/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './page-container';
 export * from './side-bar';
 export * from './canvas';
+export * from './mdx-components';
--- apps/docs/src/pages/docs/[[...slug]].tsx
+++ apps/docs/src/pages/docs/[[...slug]].tsx
@@ -2,29 +2,8 @@ import { GetStaticPaths } from 'next';
 import { allDocs, Doc } from 'contentlayer/generated';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 import { DocsLayout } from '~/layouts';
+import { mdxComponents } from '~/components';
 import { FindAnchors, FindAnchorsReturn } from '~/utils';
-import { createElement, ReactElement } from 'react';
-
-const addDocClass = (props: any, tag: any) =>
-  createElement(tag, { ...props, className: 'docs' });
-
-const mdxComponents = {
-  h1: (props: any) => addDocClass(props, 'h1'),
-  h2: (props: any) => addDocClass(props, 'h2'),
-  h3: (props: any) => addDocClass(props, 'h3'),
-  h4: (props: any) => addDocClass(props, 'h4'),
-  h5: (props: any) => addDocClass(props, 'h5'),
-  h6: (props: any) => addDocClass(props, 'h6'),
-  p: (props: any) => addDocClass(props, 'p'),
-  ol: (props: any) => addDocClass(props, 'ol'),
-  ul: (props: any) => addDocClass(props, 'ul'),
-  pre: (props: any) => addDocClass(props, 'pre'),
-  li: (props: any) => addDocClass(props, 'li'),
-  a: (props: any) => addDocClass(props, 'a'),
-  code: (props: any) => addDocClass(props, 'code'),
-  detail: (props: any) => addDocClass(props, 'detail'),
-  blockquote: (props: any) => addDocClass(props, 'blockquote')
-};
 
 export default function Page({
   doc,
