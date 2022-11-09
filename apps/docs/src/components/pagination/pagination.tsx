3fa45fcea34cb8bd01915823a6b1e1736a5bb7e5
nafees nazik <nafeesnazik21@gmail.com>
2022-11-09 20:09:32 +0530

    fix(docs): update pagination styles

--- apps/docs/src/components/pagination/pagination.tsx
+++ apps/docs/src/components/pagination/pagination.tsx
@@ -12,11 +12,11 @@ export function usePagination() {
 export function Pagination() {
   const { prev, next } = usePagination();
   return (
-    <div className="pt-8 mt-16 mb-8 border-t dark:border-neutral-800 flex flex-row items-center justify-between">
+    <div className="pt-8 mt-16 mb-8 border-t border-neutral-7 flex flex-row items-center justify-between">
       <div className="flex-1 min-w-0">
         {prev ? (
           <Link
-            className="text-lg font-medium p-4 -m-4 no-underline transition-colors text-gray-600 dark:text-gray-300 dark:hover:text-prime-500 hover:text-prime-500 flex items-center mr-2"
+            className="text-lg font-medium p-4 -m-4 no-underline transition-colors text-info-10 hover:text-info-11  flex items-center mr-2"
             rel="prev"
             href={prev.path}
           >
@@ -42,7 +42,7 @@ export function Pagination() {
         {next ? (
           <Link
             rel="next"
-            className="text-lg font-medium p-4 -m-4 no-underline transition-colors text-gray-600 dark:text-gray-300 dark:hover:text-prime-500 hover:text-prime-500 inline-flex items-center ml-2"
+            className="text-lg font-medium p-4 -m-4 no-underline transition-colors text-info-10 hover:text-info-11 inline-flex items-center ml-2"
             href={next.path}
           >
             {next.title}
