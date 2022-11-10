f3a93bfb32760a69fb1e3fdc867102d275753c1a
nafees nazik <nafeesnazik21@gmail.com>
2022-11-10 23:52:44 +0530

    fix(react): add forwarded ref to VisuallyHidden component

--- packages/react/src/visually-hidden/visually-hidden.tsx
+++ packages/react/src/visually-hidden/visually-hidden.tsx
@@ -3,10 +3,10 @@ import { cx, aphro, forwardRef, HTMLPolymorphicProps } from '~/system';
 export interface VisuallyHiddenProps extends HTMLPolymorphicProps<'span'> {}
 
 export const VisuallyHidden = forwardRef<'span', VisuallyHiddenProps>(
-  function VisuallyHidden(props) {
+  function VisuallyHidden(props, ref) {
     const { as, children, className } = props;
     return (
-      <aphro.span as={as} className={cx('sr-only', className)}>
+      <aphro.span ref={ref} as={as} className={cx('sr-only', className)}>
         {children}
       </aphro.span>
     );
