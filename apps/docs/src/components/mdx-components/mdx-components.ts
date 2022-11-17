b5a270c8c5a17f9dee92ae389d1b6767e5287767
nafees nazik <nafeesnazik21@gmail.com>
2022-11-17 23:20:02 +0530

    fix(docs): remove unnecessary mdx components

--- apps/docs/src/components/mdx-components/mdx-components.ts
+++ apps/docs/src/components/mdx-components/mdx-components.ts
@@ -1,27 +1,9 @@
 import { Canvas } from '~/components';
-import { createElement } from 'react';
 import { Center, Spinner, Button, IconButton } from '@aphrodite-ui/react';
 import { FiSearch, FiMail, FiArrowRightCircle, FiChrome } from 'react-icons/fi';
 import { MdPhone } from 'react-icons/md';
-const addDocClass = (props: any, tag: any) =>
-  createElement(tag, { ...props, className: 'docs' });
 
 export const mdxComponents = {
-  h1: (props: any) => addDocClass(props, 'h1'),
-  h2: (props: any) => addDocClass(props, 'h2'),
-  h3: (props: any) => addDocClass(props, 'h3'),
-  h4: (props: any) => addDocClass(props, 'h4'),
-  h5: (props: any) => addDocClass(props, 'h5'),
-  h6: (props: any) => addDocClass(props, 'h6'),
-  p: (props: any) => addDocClass(props, 'p'),
-  ol: (props: any) => addDocClass(props, 'ol'),
-  ul: (props: any) => addDocClass(props, 'ul'),
-  pre: (props: any) => addDocClass(props, 'pre'),
-  li: (props: any) => addDocClass(props, 'li'),
-  a: (props: any) => addDocClass(props, 'a'),
-  code: (props: any) => addDocClass(props, 'code'),
-  detail: (props: any) => addDocClass(props, 'detail'),
-  blockquote: (props: any) => addDocClass(props, 'blockquote'),
   Canvas,
   Center,
   Spinner,
