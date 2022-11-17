bfc5bda3c66118d0c21d608d4ae01ec64183cfe0
nafees nazik <nafeesnazik21@gmail.com>
2022-11-17 20:18:47 +0530

    feat(docs): add footer component

---
+++ apps/docs/src/components/footer/footer.tsx
@@ -0,0 +1,17 @@
+export const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center">
+      <p>
+        Proudly made in 🇱🇰 by{' '}
+        <a
+          className="underline"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://nafeez.xyz/"
+        >
+          Nafees Nazik.
+        </a>
+      </p>
+    </footer>
+  );
+};
