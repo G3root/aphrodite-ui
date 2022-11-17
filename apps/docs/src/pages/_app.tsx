6156a37d51c3e19833d3b516b25035746c09f9d7
nafees nazik <nafeesnazik21@gmail.com>
2022-11-17 23:36:29 +0530

    feat(docs): add default seo

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { DefaultSeo } from 'next-seo';
+import { siteConfig } from '~/configs';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <DefaultSeo {...siteConfig.seo} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
