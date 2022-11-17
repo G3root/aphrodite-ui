a2879f4ee2483f7c92e3c0acb96f7e44a49451e7
nafees nazik <nafeesnazik21@gmail.com>
2022-11-17 23:36:01 +0530

    feat(docs): add seo configs

--- apps/docs/src/configs/site-config.ts
+++ apps/docs/src/configs/site-config.ts
@@ -1,9 +1,20 @@
-const baseUrl = 'https://github.com/G3root/aphrodite-ui';
-
+const baseConfig = {
+  repo: 'https://github.com/G3root/aphrodite-ui',
+  title: 'Aphrodite UI - Rapidly prototypes without a hassle.',
+  description:
+    'A Collection of high-quality accessible UI components built with Tailwind CSS',
+  url: 'https://aphrodite-ui.vercel.com'
+};
 export const siteConfig = {
   repo: {
-    url: baseUrl,
-    editUrl: `${baseUrl}/edit/main/apps/docs/src/pages`,
-    blobUrl: `${baseUrl}/edit/main/apps/docs/src/pages`
+    url: baseConfig.url,
+    editUrl: `${baseConfig.repo}/edit/main/apps/docs/content/docs`,
+    blobUrl: `${baseConfig.repo}/blob/main/apps/docs/content/docs`
+  },
+  seo: {
+    title: baseConfig.title,
+    titleTemplate: '%s - Aphrodite-UI',
+    description: baseConfig.description,
+    siteUrl: baseConfig.url
   }
 };
