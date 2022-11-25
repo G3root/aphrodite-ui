e199cfd3e23834afca1ce03aac8315c1d98c359d
nafees nazik <nafeesnazik21@gmail.com>
2022-11-25 19:11:51 +0530

    feat: export components

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,3 +4,5 @@ export * from './icon-button';
 export * from './visually-hidden';
 export * from './system';
 export * from './spinner';
+export * from './tag';
+export * from './icon';
---
+++ packages/react/src/tag/index.ts
@@ -0,0 +1,5 @@
+export * from './tag';
+export * from './tag-close-button';
+export * from './tag-icon';
+export * from './tag-label';
+export * from './tag-context';
